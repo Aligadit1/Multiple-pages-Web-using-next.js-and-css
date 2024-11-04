ae44e32daa6a00607da27dd3e7ac6eb21fa0832b
ali <aligadit9192@gmail.com>
2024-11-05 01:33:52 +0500

    Web Completed

--- my-app/src/Components/Header.tsx
+++ my-app/src/Components/Header.tsx
@@ -1,15 +1,24 @@
-import React from 'react'
+"use client"
+import React, {useRef } from 'react'
 import Link from 'next/link'
-function Header() {
+function Header(){  
+const headerRef =useRef<HTMLUListElement>(null);
+const toggleHeader = ()=>{
+  headerRef.current?.classList.toggle("header-li")
+}
   return (
-    <div>
-        <ul className='header'>
+    <div className='header-div'>
+        <ul className='header header-li' id='navbar' ref={headerRef}>
             <Link href="/"><li>Home</li></Link>
             <Link href="/about"><li>About</li></Link>
             <Link href="/contact-us"><li>Contact Us</li></Link>
             <Link href="/services"><li>Services</li></Link>
         </ul>
-      
+          <div className='hamburgerDiv' onClick={toggleHeader}>
+        <div className='hamburger hamburger1'></div>
+        <div className='hamburger hamburger2'></div>
+        <div className='hamburger hamburger3'></div>
+        </div>
     </div>
   )
 }
